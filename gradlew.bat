1693e14a8b4ac4f4e0efed29ea307f94a7a264b2
DFkaka <DFkaka@users.noreply.github.com>
2026-05-21 09:43:21 +0800

    Fix: remove BOM from gradlew, fix JVM opts quoting

--- gradlew.bat
+++ gradlew.bat
@@ -1,5 +1,4 @@
-﻿@rem Gradle startup script for Windows
-
+@rem Gradle startup script for Windows
 @if "%DEBUG%" == "" @echo off
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
@@ -9,8 +8,7 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Add default JVM options here.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -20,11 +18,7 @@ set JAVA_EXE=java.exe
 if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
+echo ERROR: JAVA_HOME is not set and no java command could be found in your PATH.
 goto fail
 
 :findJavaFromJavaHome
@@ -35,22 +29,13 @@ if exist "%JAVA_EXE%" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
 goto fail
 
 :execute
-@rem Setup the command line
-
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-@rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-@rem End local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
